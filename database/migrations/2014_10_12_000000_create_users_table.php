9b8e95601ec07be00e464ce5b1d7c7a1130a323a
asd <402442701@qq.com>
2017-12-16 15:39:23 +0800

    settle conflict

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-<<<<<<< HEAD
             $table->string('nickname',32)->unique()->notnull();
             $table->string('password',255)->notnull();
             $table->integer('role_id')->default(1);
@@ -24,12 +23,6 @@ class CreateUsersTable extends Migration
             $table->integer('created_user_id')->default(1);
             $table->integer('updated_user_id')->default(1);
             $table->tinyInteger('flag')->defalut(1);
-=======
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
->>>>>>> 6e0e8259bbaf1b197e2f92cfdab0a75cb5254a9c
             $table->timestamps();
         });
     }
